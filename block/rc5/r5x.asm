1986275a7a88bbd52596eb77867bd26da167e92d
odzhan <odzhan@protonmail.com>
2018-01-23 13:06:17 +0000

    upd

--- block/rc5/r5x.asm
+++ block/rc5/r5x.asm
@@ -32,7 +32,7 @@
 ;
 ; https://people.csail.mit.edu/rivest/Rivest-rc5rev.pdf
 ;
-; size: 114 bytes for single call -- trashes the key buffer --
+; size: 120 bytes for single call
 ;
 ; xrc5_cryptx(void *key, void *data)
 ;
@@ -69,73 +69,79 @@ endstruc
 _xrc5_cryptx:
 xrc5_cryptx:
     pushad
-    mov    ebx, [esp+32+4]    ; edi = key / L
-    mov    esi, [esp+32+8]    ; esi = data
-    xor    ecx, ecx           ; ecx = 0
-    mov    cl, RC5_KR*4       ; allocate space for sub keys
-    sub    esp, ecx           ; esp = S
-    ; initialize S / sub keys    
-    mov    edi, esp           ; edi = S
-    mov    eax, 0xB7E15163    ; eax = RC6_P
+    mov    esi, [esp+32+4]     ; edi = key / L
+    mov    ebx, [esp+32+8]     ; esi = data
+    xor    ecx, ecx            ; ecx = 0
+    mov    cl, RC5_KR*4+16     ; allocate space for key and sub keys
+    sub    esp, ecx            ; esp = S
+    ; copy 128-bit to local buffer
+    mov    edi, esp            ; edi = L
+    movsd
+    movsd
+    movsd
+    movsd
+    ; initialize S / sub keys 
+    push   edi                 ; save S
+    mov    eax, 0xB7E15163     ; eax = RC6_P
     mov    cl, RC5_KR    
 init_subkeys:
-    stosd                     ; S[i] = A
-    add    eax, 0x9E3779B9    ; A += RC6_Q
+    stosd                      ; S[i] = A
+    add    eax, 0x9E3779B9     ; A += RC6_Q
     loop   init_subkeys
-    mov    edi, ebx
-    mul    ecx                ; eax = 0, edx = 0
-    xor    ebx, ebx           ; ebx = 0
+    pop    edi                 ; restore S
+    mov    esi, ebx            ; esi = data
+    mul    ecx                 ; eax = 0, edx = 0
+    xor    ebx, ebx            ; ebx = 0
 set_idx:    
-    xor    ebp, ebp           ; i % RC6_KR    
+    xor    ebp, ebp            ; i % RC6_KR    
 init_key_loop:
     cmp    ebp, RC5_KR
     je     set_idx    
 
     ; A = S[i%RC6_KR] = ROTL32(S[i%RC5_KR] + A+B, 3); 
-    add    eax, ebx           ; A += B
-    add    eax, [esp+ebp*4]   ; A += S[i%RC5_KR]
-    rol    eax, 3             ; A  = ROTL32(A, 3)
-    mov    [esp+ebp*4], eax   ; S[i%RC6_KR] = A
+    add    eax, ebx            ; A += B
+    add    eax, [edi+ebp*4]    ; A += S[i%RC5_KR]
+    rol    eax, 3              ; A  = ROTL32(A, 3)
+    mov    [edi+ebp*4], eax    ; S[i%RC6_KR] = A
     
     ; B = L[i%4] = ROTL32(L[i%4] + A+B, A+B);
-    add    ebx, eax           ; B += A
-    mov    ecx, ebx           ; save A+B in ecx
-    push   edx                ; save i
-    and    dl, 3              ; %= 4
-    add    ebx, [edi+edx*4]   ; B += L[i%4]    
-    rol    ebx, cl            ; B = ROTL32(B, A+B)
-    mov    [edi+edx*4], ebx   ; L[i%4] = B    
-    pop    edx                ; restore i    
+    add    ebx, eax            ; B += A
+    mov    ecx, ebx            ; save A+B in ecx
+    push   edx                 ; save i
+    and    dl, 3               ; %= 4
+    add    ebx, [edi+edx*4-16]    ; B += L[i%4]    
+    rol    ebx, cl             ; B = ROTL32(B, A+B)
+    mov    [edi+edx*4-16], ebx    ; L[i%4] = B    
+    pop    edx                 ; restore i    
     inc    ebp
-    inc    edx                ; i++
-    cmp    dl, RC5_KR*3       ; i<RC6_KR*3
-    jnz    init_key_loop
+    inc    edx                 ; i++
+    cmp    dl, RC5_KR*3        ; i<RC6_KR*3
+    jnz    init_key_loop    
     
-    mov    edi, esp 
-    push   esi                ; save ptr to data    
-    lodsd                     ; eax = x->w[0]
-    add    eax, [edi]         ; A  += *k; k++;
+    push   esi                 ; save ptr to data    
+    lodsd                      ; eax = x->w[0]
+    add    eax, [edi]          ; A  += *k; k++;
     scasd
-    xchg   eax, ebx           ; XCHG(A, B);
-    lodsd                     ; eax = x->w[1]
+    xchg   eax, ebx            ; XCHG(A, B);
+    lodsd                      ; eax = x->w[1]
     mov    dl, RC5_KR - 1   
     jmp    mix_key
 enc_loop:
     ; A = ROTL32(A ^ B, B) + *k*; k++;
-    xor   eax, ebx            ; A ^= B 
-    mov   ecx, ebx            ; ecx = B 
-    rol   eax, cl             ; A = ROTL32(A ^ B, B)
-mix_key:    
-    add   eax, [edi]          ; A += *k; k++;
+    xor   eax, ebx             ; A ^= B 
+    mov   ecx, ebx             ; ecx = B 
+    rol   eax, cl              ; A = ROTL32(A ^ B, B)
+mix_key:     
+    add   eax, [edi]           ; A += *k; k++;
     scasd
-    xchg  eax, ebx            ; XCHG(A, B);
+    xchg  eax, ebx             ; XCHG(A, B);
     dec   edx
     jnz   enc_loop    
     
-    pop   esp                 ; restore ptr to data    
+    pop   esp                  ; restore ptr to data    
     xchg  esp, edi
-    stosd                     ; x->w[0] = A
+    stosd                      ; x->w[0] = A
     xchg  eax, ebx
-    stosd                     ; x->w[1] = B         
+    stosd                      ; x->w[1] = B         
     popad
     ret
